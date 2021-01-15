7a451325133a17164abf02d45702618c2be07552
duongth <thanhduong9613@gmail.com>
2021-01-15 15:22:12 +0700

    update

--- upload/article/controller/common/home.php
+++ upload/article/controller/common/home.php
@@ -13,8 +13,23 @@ class ControllerCommonHome extends Controller {
 		// $data['column_right'] = $this->load->controller('common/column_right');
 		// $data['content_top'] = $this->load->controller('common/content_top');
 		// $data['content_bottom'] = $this->load->controller('common/content_bottom');
-		// $data['footer'] = $this->load->controller('common/footer');
-		// $data['header'] = $this->load->controller('common/header');
+		$data['footer'] = $this->load->controller('common/footer');
+		$data['header'] = $this->load->controller('common/header');
+
+		$images = array(
+			'jane' => DIR_IMAGE."jane.jpg"
+			// 'avatar_girl2'        => $location_info['name'],
+			// 'avatar_smoke'     => nl2br($location_info['address']),
+			// 'bandmember'     => $location_info['geocode'],
+			// 'girl_hat'   => $location_info['telephone'],
+			// 'jeans'         => $location_info['fax'],
+			// 'man_hat'       => $image,
+			// 'runway'        => nl2br($location_info['open']),
+			// 'team1'     => $location_info['comment'],
+			// 'team4'     => $location_info['comment'],
+			// 'workshop'     => $location_info['comment']
+		);
+		$data["images"] = $images;
 
 		$this->response->setOutput($this->load->view('common/home', $data));
 	}
